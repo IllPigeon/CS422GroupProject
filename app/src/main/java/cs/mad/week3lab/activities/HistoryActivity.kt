7b5307a3509d42f986c371fc9e7317dc8abbcf01
JacobButcher-Github <122327437+JacobButcher-Github@users.noreply.github.com>
2024-07-26 19:38:29 -0500

    ??

--- app/src/main/java/cs/mad/week3lab/activities/HistoryActivity.kt
+++ app/src/main/java/cs/mad/week3lab/activities/HistoryActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class HistoryActivity: AppCompatActivity() {
+    private lateinit var binding:
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
