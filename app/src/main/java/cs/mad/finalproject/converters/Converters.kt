0d20c8fced923565b75dc415a0555b501a75d917
JacobButcher-Github <122327437+JacobButcher-Github@users.noreply.github.com>
2024-07-31 00:02:35 -0500

    Add converter to convert json to list of strings

--- app/src/main/java/cs/mad/finalproject/converters/Converters.kt
+++ app/src/main/java/cs/mad/finalproject/converters/Converters.kt
@@ -1,8 +1,17 @@
 package cs.mad.finalproject.converters
 
 import androidx.room.TypeConverter
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
 class Converters {
     @TypeConverter
-    fun fromString(value: String) 
+    fun fromString(value: String): List<String> {
+        val listType = object : TypeToken<List<String>>() {}.type
+        return Gson().fromJson(value, listType)
+    }
+
+    fun fromList(list: List<String>): String {
+        return Gson().toJson(list)
+    }
 }
